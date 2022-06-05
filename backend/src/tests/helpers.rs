b49cf8f10440475db48b7479b004b7fd623a14df
Scipex <scipex.dev@gmail.com>
2022-06-05 20:19:51 +0300

    Simplified creating and sending test requests

--- backend/src/tests/helpers.rs
+++ backend/src/tests/helpers.rs
@@ -1,8 +1,52 @@
 use super::testdb::TestDB;
+use actix_web::{
+    body::MessageBody,
+    dev::{Service, ServiceResponse},
+    test::{call_and_read_body_json, TestRequest},
+};
+use serde::Serialize;
+use serde_json::Value;
 
 pub async fn test_db() -> TestDB {
     dotenv::dotenv().ok();
-    pretty_env_logger::init();
+    pretty_env_logger::try_init().ok();
 
     TestDB::new().await
+}
+
+pub fn get(url: &str) -> Request {
+    Request::GET {
+        url: url.to_string(),
+    }
+}
+
+pub fn post<T: Serialize>(url: &str, body: T) -> Request {
+    Request::POST {
+        url: url.to_string(),
+        json: serde_json::to_value(body).unwrap(),
+    }
+}
+
+pub enum Request {
+    GET { url: String },
+    POST { url: String, json: Value },
+}
+
+impl Request {
+    pub async fn send<S, B>(&self, server: &S) -> Value
+    where
+        S: Service<actix_http::Request, Response = ServiceResponse<B>, Error = actix_web::Error>,
+        B: MessageBody,
+    {
+        match self {
+            Request::GET { url } => {
+                let req = TestRequest::get().uri(&url).to_request();
+                return call_and_read_body_json(server, req).await;
+            }
+            Request::POST { url, json } => {
+                let req = TestRequest::post().uri(&url).set_json(json).to_request();
+                return call_and_read_body_json(server, req).await;
+            }
+        }
+    }
 }
--- backend/src/tests/mod.rs
+++ backend/src/tests/mod.rs
@@ -2,7 +2,7 @@ use crate::{hello, users, AppData, UserInfo};
 use actix_web::{middleware::Logger, test, web, App};
 use assert_json_diff::{assert_json_eq, assert_json_include};
 use helpers::*;
-use serde_json::{json, Value};
+use serde_json::json;
 
 mod helpers;
 mod testdb;
@@ -19,23 +19,45 @@ async fn insert_user() {
     )
     .await;
 
-    let req = test::TestRequest::get().uri("/users").to_request();
-    let resp: Value = test::call_and_read_body_json(&server, req).await;
+    let resp = get("/users").send(&server).await;
     assert_json_eq!(resp, json!([]));
 
     let user = UserInfo {
         username: "bob".to_string(),
     };
 
-    let req = test::TestRequest::post()
-        .uri("/users")
-        .set_json(json!(user))
-        .to_request();
-    let resp: Value = test::call_and_read_body_json(&server, req).await;
+    let resp = post("/users", &user).send(&server).await;
     assert_json_eq!(resp, json!(user));
 
-    let req = test::TestRequest::get().uri("/users").to_request();
-    let resp: Value = test::call_and_read_body_json(&server, req).await;
+    let resp = get("/users").send(&server).await;
+    assert_json_include!(actual: resp, expected: json!([user]));
+
+    db.drop_db().await;
+}
+
+#[actix_web::test]
+async fn insert_user_1() {
+    let db = test_db().await;
+    let server = test::init_service(
+        App::new()
+            .wrap(Logger::default())
+            .app_data(web::Data::new(AppData { db_pool: db.pool() }))
+            .service(hello)
+            .service(users),
+    )
+    .await;
+
+    let resp = get("/users").send(&server).await;
+    assert_json_eq!(resp, json!([]));
+
+    let user = UserInfo {
+        username: "alice".to_string(),
+    };
+
+    let resp = post("/users", &user).send(&server).await;
+    assert_json_eq!(resp, json!(user));
+
+    let resp = get("/users").send(&server).await;
     assert_json_include!(actual: resp, expected: json!([user]));
 
     db.drop_db().await;
